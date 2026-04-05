b999a254c7f9ab3ef8eccdb1a36ed68e0406b432
nikitapolsenko <polsenkonikita@gmail.com>
2026-04-05 19:13:46 +0100

    feat: implement CI/CD pipeline and GitHub Pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ci-cd-lr-project/',
+  base: '/Lab_6/',
   build: {
     rollupOptions: {
       output: {
